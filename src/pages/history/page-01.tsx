7bab036fe9366882147687a011b7d3595d176be1
hykim <hykim@gccare.net>
2026-01-28 14:28:28 +0900

    favi

--- src/pages/history/page-01.tsx
+++ src/pages/history/page-01.tsx
@@ -1,10 +1,12 @@
 import TypingText from '@/components/typingText';
+import icoMan from '@/assets/images/svg/ico-man.svg';
 import { Box, Stack, Typography } from '@mui/material';
 
 export default function Page01() {
   return (
     <Box className="mainPage page-01">
       <Stack spacing={2} alignItems="center">
+        <Box component="img" src={icoMan} alt="Man icon" sx={{ width: 160, height: 'auto' }} />
         <Typography variant="h3" component="h1">
           Page 01
         </Typography>
