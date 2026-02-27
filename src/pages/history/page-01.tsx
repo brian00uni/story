49d148e7fb54e4c15a20dd3a46274d3aacf1d7ab
hykim <hykim@gccare.net>
2026-02-27 16:35:37 +0900

    v 0.25

--- src/pages/history/page-01.tsx
+++ src/pages/history/page-01.tsx
@@ -2,7 +2,12 @@ import icoMan from '@/assets/images/svg/ico-man.svg';
 import TypingText from '@/components/typingText';
 import { Box, Stack, Typography } from '@mui/material';
 
+import animationData from '@/data/intro.json'; // 로컬 JSON 파일
+import Lottie from 'lottie-react';
+import { useState } from 'react';
+
 export default function Page01() {
+  const [isPlaying, setIsPlaying] = useState(true);
   return (
     <Box className="mainPage page-01">
       <Stack spacing={2} alignItems="center">
@@ -11,6 +16,16 @@ export default function Page01() {
         <Typography variant="h3" component="h1">
           Page 01
         </Typography>
+
+        <Box>
+          <Lottie
+            animationData={animationData}
+            loop={true}
+            autoplay={isPlaying}
+            style={{ width: 500, height: 'auto' }}
+          />
+          <button onClick={() => setIsPlaying(!isPlaying)}>{isPlaying ? '정지' : '재생'}</button>
+        </Box>
 
         {/* <img src={lgPhotoshop} alt="Photoshop logo" /> */}
         <TypingText />
